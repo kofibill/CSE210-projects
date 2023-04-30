fb017c4274ef2f056d67579720642c8e12739a24
kofibill <jessekim064@gmail.com>
2023-04-29 23:55:01 -0600

    trials

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,14 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.Write("what is your first name? ");
+        string firstname=Console.ReadLine();
+
+        Console.Write("what is your last name? ");
+        string lastname=Console.ReadLine();
+
+        Console.WriteLine($"your full name is {lastname}, {firstname} {lastname}");
+
+        
     }
 }
